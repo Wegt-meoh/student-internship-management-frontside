b29aedd8fc8e8bbf520dc358bce9f376107acf59
lasting <lastingcoder@qq.com>
2023-04-28 22:06:47 +0800

    add: post detail modal in student find post

--- src/app/(home)/student/main/find-post/page.tsx
+++ src/app/(home)/student/main/find-post/page.tsx
@@ -3,13 +3,16 @@
 import { findAllPost } from "@/api/post";
 import { PostWithCreatedUserVo } from "@/api/post/index.type";
 import { createRequestPost } from "@/api/requestPost";
-import { Button, message, Table } from "antd";
+import PostDetail from "@/ui/postDetail";
+import { Button, message, Modal, Table } from "antd";
 import React, { useEffect, useState } from "react";
 
 export default function Page() {
   const [tableData, setTableData] = useState<PostWithCreatedUserVo[]>([]);
   const [loading, setLoading] = useState(true);
   const [buttonDisable, setButtonDisable] = useState(false);
+  const [modalOpen, setModalOpen] = useState(false);
+  const [postId, setPostId] = useState();
 
   async function fetcheTableData() {
     setLoading(true);
@@ -37,6 +40,16 @@ export default function Page() {
 
   return (
     <div>
+      <Modal
+        footer={null}
+        title="岗位详情"
+        open={modalOpen}
+        onCancel={() => {
+          setModalOpen(false);
+        }}
+      >
+        {postId && <PostDetail postId={postId} />}
+      </Modal>
       <Table
         loading={loading}
         dataSource={tableData}
@@ -64,6 +77,19 @@ export default function Page() {
             filterSearch: true,
             onFilter: (value, record) => {
               return record.name.includes(value + "");
+            },
+            render: (_, record) => {
+              return (
+                <span
+                  onClick={() => {
+                    setModalOpen(true);
+                    setPostId(record.id);
+                  }}
+                  className=" hover:underline hover:cursor-pointer"
+                >
+                  {record.name}
+                </span>
+              );
             },
           },
           {
